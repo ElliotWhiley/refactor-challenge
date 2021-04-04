a41679515b02c5cee97eb9d8742e306fe36c230b
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-04 13:15:58 +1200

    Refactor implementation to trim full names to maximum length of 255, making test case pass

--- Review/People.cs
+++ Review/People.cs
@@ -5,6 +5,8 @@ namespace Review
     public class People
     {
         private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
+        private const int MaximumFullNameLength = 255;
+
         public string Name { get; }
         public DateTimeOffset DateOfBirth { get; }
 
@@ -23,11 +25,13 @@ namespace Review
             if (lastName.Contains("test")) {
                 return Name;
             }
-            if ((Name.Length + lastName).Length > 255) {
-                (Name + " " + lastName).Substring(0, 255);
+
+            var fullName = $"{Name} {lastName}";
+            if (fullName.Length > MaximumFullNameLength) {
+                return fullName.Substring(0, MaximumFullNameLength);
             }
 
-            return Name + " " + lastName;
+            return fullName;
         }
     }
 }
