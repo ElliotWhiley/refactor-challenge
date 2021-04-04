950ece134ff503f76083d0cc5f8443ee725f878e
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-04 17:17:10 +1200

    Remove references to BirthingUnit, using person constructor directly

--- Tests/PeopleTests.cs
+++ Tests/PeopleTests.cs
@@ -1,16 +1,19 @@
-﻿using System.Linq;
-using Review;
+﻿using Review;
 using Xunit;
 
 namespace Tests
 {
     public class PeopleTests
     {
+        private const string LongLastNameWith256Characters = "Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                                                             "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                                                             "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
+                                                             "oooooooooooooooooooooong";
+
         [Fact]
         public void GetMarriedReturnsFullName()
         {
-            var birthingUnit = new BirthingUnit();
-            var person = birthingUnit.GetPeople(1).Single();
+            var person = new People("Tester");
             var fullName = person.GetMarried( "Smith");
             Assert.True(fullName == $"{person.Name} Smith");
         }
@@ -18,22 +21,16 @@ namespace Tests
         [Fact]
         public void GetFullNameTrimsLongNames()
         {
-            var birthingUnit = new BirthingUnit();
-            var person = birthingUnit.GetPeople(1).Single();
-            const string longLastNameWith256Characters = "Loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-                                                         "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-                                                         "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo" +
-                                                         "oooooooooooooooooooooong";
-            person.GetMarried(longLastNameWith256Characters);
-            Assert.True(person.LastName == longLastNameWith256Characters);
+            var person = new People("Tester");
+            person.GetMarried(LongLastNameWith256Characters);
+            Assert.True(person.LastName == LongLastNameWith256Characters);
             Assert.True(person.GetFullName().Length == 255);
         }
 
         [Fact]
         public void GetMarriedWithTestLastNameReturnsFirstName()
         {
-            var birthingUnit = new BirthingUnit();
-            var person = birthingUnit.GetPeople(1).Single();
+            var person = new People("Tester");
             var fullName = person.GetMarried("test");
             Assert.True(fullName == person.Name);
         }
@@ -41,8 +38,7 @@ namespace Tests
         [Fact]
         public void GetMarriedUpdatesLastName()
         {
-            var birthingUnit = new BirthingUnit();
-            var person = birthingUnit.GetPeople(1).Single();
+            var person = new People("Tester");
             person.GetMarried( "Smith");
             Assert.True(person.LastName == "Smith");
             Assert.True(person.GetFullName() == $"{person.Name} Smith");
