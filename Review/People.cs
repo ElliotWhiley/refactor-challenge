11841870d2c59a7e7491f2593905a81e930ce572
ElliotWhiley <elliot.whiley94@gmail.com>
2021-04-04 12:52:38 +1200

    Name and DateOfBirth are only set from the constructor, remove redundant private set

--- Review/People.cs
+++ Review/People.cs
@@ -5,8 +5,8 @@ namespace Review
     public class People
     {
         private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
-        public string Name { get; private set; }
-        public DateTimeOffset DateOfBirth { get; private set; }
+        public string Name { get; }
+        public DateTimeOffset DateOfBirth { get; }
 
         public People(string name) : this(name, Under16.Date)
         {
